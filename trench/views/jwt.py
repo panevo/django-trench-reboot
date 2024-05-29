c99d4be898bf0a5190fcfa058a1b51a90740945c
Karlo Krakan <karlo.krakan@panevo.com>
2024-05-29 15:18:54 -0600

    fix: Linting errors

--- trench/views/jwt.py
+++ trench/views/jwt.py
@@ -1,13 +1,14 @@
 from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
 
 from trench.views import MFAFirstStepMixin, MFASecondStepMixin, MFAStepMixin, User
 
 
 class MFAJWTView(MFAStepMixin):
     def _successful_authentication_response(self, user: User) -> Response:
-        token = RefreshToken.for_user(user=user)
-        return Response(data={"refresh": str(token), "access": str(token.access_token)})
+        refresh_token = RefreshToken.for_user(user=user)
+        access_token = AccessToken.for_user(user=user)
+        return Response(data={"refresh": str(refresh_token), "access": str(access_token)})
 
 
 class MFAFirstStepJWTView(MFAJWTView, MFAFirstStepMixin):
