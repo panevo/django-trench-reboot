ecdbd6efa920fd8da4f68a105559506f44aad92f
Karlo Krakan <karlo.krakan@panevo.com>
2024-05-30 10:46:16 -0600

    fix: Test suite

--- trench/views/jwt.py
+++ trench/views/jwt.py
@@ -1,14 +1,13 @@
 from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from trench.views import MFAFirstStepMixin, MFASecondStepMixin, MFAStepMixin, User
 
 
 class MFAJWTView(MFAStepMixin):
     def _successful_authentication_response(self, user: User) -> Response:
-        refresh_token = RefreshToken.for_user(user=user)
-        access_token = AccessToken.for_user(user=user)
-        return Response(data={"refresh": str(refresh_token), "access": str(access_token)})
+        token: RefreshToken = RefreshToken.for_user(user=user)
+        return Response(data={"refresh": str(token), "access": str(token.access_token)})
 
 
 class MFAFirstStepJWTView(MFAJWTView, MFAFirstStepMixin):
